58f52d86c9aeb2227aadf3e804cfd0316e2dd200
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2024-08-20 22:36:37 +0400

    Update silgiuserbot.py

--- userbot/modules/silgiuserbot.py
+++ userbot/modules/silgiuserbot.py
@@ -6,4 +6,4 @@ from userbot.events import register
 @register(incoming=True, from_users=BRAIN_CHECKER, pattern="^.ualive$")
 @register(incoming=True, from_users=WHITELIST, pattern="^.ualive$")
 async def _(q):
-    await q.client.send_message(q.chat_id,"`[⚝ 𝑺𝑰𝑳𝑮𝑰 𝑼𝑺𝑬𝑹𝑩𝑶𝑻 ⚝](@silgiuserbot)💻 Online`")
+    await q.client.send_message(q.chat_id,"`⚝ 𝑺𝑰𝑳𝑮𝑰 𝑼𝑺𝑬𝑹𝑩𝑶𝑻 ⚝💻 Online`")
