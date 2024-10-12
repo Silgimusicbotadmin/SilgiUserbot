0d2c24deaa2b733f3f195b929671d1e7ea8d7c1b
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2024-10-12 09:07:35 +0400

    Update silgiuserbot.py

--- userbot/modules/silgiuserbot.py
+++ userbot/modules/silgiuserbot.py
@@ -3,7 +3,7 @@ from telethon import events
 from userbot import BRAIN_CHECKER, WHITELIST
 from userbot.events import register
 
-@register(incoming=True, from_users=BRAIN_CHECKER, pattern="^.ualive$")
+
 @register(incoming=True, from_users=WHITELIST, pattern="^.ualive$")
 async def _(q):
     await q.client.send_message(q.chat_id,"`⚝ 𝑺𝑰𝑳𝑮𝑰 𝑼𝑺𝑬𝑹𝑩𝑶𝑻 ⚝💻 Online`")
