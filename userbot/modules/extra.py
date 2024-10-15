af94bc12fe53411d44d51e22779afacc947dcfaa
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2024-10-15 22:59:27 +0400

    Update extra.py

--- userbot/modules/extra.py
+++ userbot/modules/extra.py
@@ -107,7 +107,7 @@ async def text2doc(event):
         with open(metn, "w") as fayl:
             fayl.write(cvb.message)
         await event.delete()
-        await event.client.send_file(event.chat_id, metn, caption="[⚝ 𝑺𝑰𝑳𝑮𝑰 𝑼𝑺𝑬𝑹𝑩𝑶𝑻 ⚝](t.me/silgiuserbot)", force_document=True)
+        await event.client.send_file(event.chat_id, metn, caption="[⚝ 𝑺𝑰𝑳𝑮𝑰 𝑼𝑺𝑬𝑹𝑩𝑶𝑻 ⚝](https://t.me/silgiub)", force_document=True)
         os.remove(metn)
     else:
         await event.edit((LANG['T4']))
