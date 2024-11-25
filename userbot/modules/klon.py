b95d6380002f872e3e6af56520638b9a38d58448
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2024-11-25 23:33:02 +0400

    Update klon.py

--- userbot/modules/klon.py
+++ userbot/modules/klon.py
@@ -1,11 +1,12 @@
-import os
 from telethon.tl.functions.photos import GetUserPhotosRequest, UploadProfilePhotoRequest, DeletePhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.functions.account import UpdateProfileRequest
 from telethon.tl.types import InputPhoto
 from telethon.errors.rpcerrorlist import PhotoCropSizeSmallError
 from userbot.events import register
 from userbot import BRAIN_CHECKER, WHITELIST
 from userbot.language import get_value
+
 LANG = get_value("userbot")
 original_profile = {
     "first_name": None,
@@ -17,9 +18,11 @@ original_profile = {
 @register(pattern="^\.klon(?: |$)(.*)", outgoing=True)
 async def klon(event):
     if event.reply_to_msg_id:
+        # Yanıtlanan mesajdaki kullanıcıyı al
         reply_message = await event.get_reply_message()
         replied_user = await event.client.get_entity(reply_message.sender_id)
     else:
+        # Manuel giriş yapılmışsa
         args = event.pattern_match.group(1)
         if args.isdigit():
             replied_user = await event.client.get_entity(int(args))
@@ -29,31 +32,40 @@ async def klon(event):
             await event.edit("🔴 İstifadəçi seçilmədi.")
             return
 
+    # BRAIN_CHECKER ve WHITELIST kontrolü
     if replied_user.id in BRAIN_CHECKER or replied_user.id in WHITELIST:
         await event.edit(LANG['SILGI'])
         return
 
     global original_profile
 
+    # Orijinal profil bilgilerini kaydet
     if original_profile["first_name"] is None:
         me = await event.client.get_me()
         original_profile["first_name"] = me.first_name
         original_profile["last_name"] = me.last_name
-        original_profile["about"] = (await event.client(functions.account.GetPrivacySettingsRequest())).bio
 
-        photos = await event.client(GetUserPhotosRequest(user_id='me', offset=0, max_id=0, limit=1))
+        # Kullanıcının biyografisini al
+        full_me = await event.client(GetFullUserRequest(me.id))
+        original_profile["about"] = full_me.about
+
+        # Profil fotoğrafını al
+        photos = await event.client(GetUserPhotosRequest(user_id="me", offset=0, max_id=0, limit=1))
         if photos.photos:
             original_profile["photo"] = photos.photos[0]
 
-    # Kullanıcının profil bilgilerini al ve uygula
+    # Klonlama işlemini başlat
     await event.edit("🔄 Klonlama prosesi başladı...")
     try:
+        full_user = await event.client(GetFullUserRequest(replied_user.id))
+
         await event.client(UpdateProfileRequest(
-            first_name=replied_user.first_name,
-            last_name=replied_user.last_name,
-            about=(await event.client(functions.account.GetPrivacySettingsRequest(replied_user))).bio
+            first_name=full_user.user.first_name,
+            last_name=full_user.user.last_name,
+            about=full_user.about
         ))
 
+        # Kullanıcının profil fotoğrafını al ve ayarla
         photos = await event.client(GetUserPhotosRequest(user_id=replied_user.id, offset=0, max_id=0, limit=1))
         if photos.photos:
             photo = photos.photos[0]
@@ -93,7 +105,7 @@ async def revert(event):
     else:
         await event.client(DeletePhotosRequest(await event.client.get_profile_photos('me')))
 
-    
+    # Orijinal profil bilgilerini sıfırla
     original_profile = {
         "first_name": None,
         "last_name": None,
