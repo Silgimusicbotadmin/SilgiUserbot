09e0c3cb23c964547aad1b2a9d550c038cc37329
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2025-02-08 19:22:38 +0400

    Update status.py

--- userbot/modules/status.py
+++ userbot/modules/status.py
@@ -1,5 +1,5 @@
 
-from userbot import CMD_HELP, ASYNC_POOL, tgbot, SPOTIFY_DC, G_DRIVE_CLIENT_ID, lastfm, LYDIA_API_KEY, YOUTUBE_API_KEY, OPEN_WEATHER_MAP_APPID, AUTO_PP, REM_BG_API_KEY, OCR_SPACE_API_KEY, PM_AUTO_BAN, BOTLOG_CHATID, DTO_VERSION
+from userbot import CMD_HELP, ASYNC_POOL, tgbot, SPOTIFY_DC, G_DRIVE_CLIENT_ID, lastfm, LYDIA_API_KEY, YOUTUBE_API_KEY, OPEN_WEATHER_MAP_APPID, AUTO_PP, REM_BG_API_KEY, OCR_SPACE_API_KEY, PM_AUTO_BAN, BOTLOG_CHATID, SILGI_VERSION
 from userbot.events import register
 from telethon import version
 from platform import python_version
@@ -43,7 +43,7 @@ async def durums(event):
 **Python {LANG['VERSION']}:** `{python_version()}`
 **TeleThon {LANG['VERSION']}:** `{version.__version__}`
 **{LANG['PLUGIN_COUNT']}:** `{len(CMD_HELP)}`
-**⚝ 𝑺𝑰𝑳𝑮𝑰 𝑼𝑺𝑬𝑹𝑩𝑶𝑻 ⚝ {LANG['VERSION']}:** `{DTO_VERSION}`
+**⚝ 𝑺𝑰𝑳𝑮𝑰 𝑼𝑺𝑬𝑹𝑩𝑶𝑻 ⚝ {LANG['VERSION']}:** `{SILGI_VERSION}`
     """)
 
 CmdHelp('status').add_command(
