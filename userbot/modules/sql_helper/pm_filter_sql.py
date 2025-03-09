8f6f9e590360c0e0a3b767a3931dbd48578f7f55
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2025-03-10 00:11:42 +0400

    Yeni özəllik😍

--- userbot/modules/sql_helper/pm_filter_sql.py
+++ userbot/modules/sql_helper/pm_filter_sql.py
@@ -4,34 +4,33 @@ try:
 except ImportError:
     raise AttributeError
 from sqlalchemy import Column, String, UnicodeText, Integer
+
 class PMFilters(BASE):
     __tablename__ = "pm_filters"
-    chat_id = Column(String(14), primary_key=True)
-    keyword = Column(UnicodeText, primary_key=True)
+    keyword = Column(String(50), primary_key=True)
     reply = Column(UnicodeText)
     f_mesg_id = Column(Integer, nullable=True)
 
-    def __init__(self, chat_id, keyword, reply, f_mesg_id):
-        self.chat_id = str(chat_id)
+    def __init__(self, keyword, reply, f_mesg_id):
         self.keyword = keyword
         self.reply = reply
         self.f_mesg_id = f_mesg_id
 
 PMFilters.__table__.create(checkfirst=True)
 
-def add_pm_filter(chat_id, keyword, reply, f_mesg_id=None):
-    filter_exists = SESSION.query(PMFilters).get((str(chat_id), keyword))
+def add_pm_filter(keyword, reply, f_mesg_id=None):
+    filter_exists = SESSION.query(PMFilters).get(keyword)
     if filter_exists:
         SESSION.delete(filter_exists)
-    new_filter = PMFilters(str(chat_id), keyword, reply, f_mesg_id)
+    new_filter = PMFilters(keyword, reply, f_mesg_id)
     SESSION.add(new_filter)
     SESSION.commit()
 
-def get_pm_filters(chat_id):
-    return SESSION.query(PMFilters).filter(PMFilters.chat_id == str(chat_id)).all()
+def get_pm_filters():
+    return SESSION.query(PMFilters).all()
 
-def remove_pm_filter(chat_id, keyword):
-    filter_exists = SESSION.query(PMFilters).get((str(chat_id), keyword))
+def remove_pm_filter(keyword):
+    filter_exists = SESSION.query(PMFilters).get(keyword)
     if filter_exists:
         SESSION.delete(filter_exists)
         SESSION.commit()
