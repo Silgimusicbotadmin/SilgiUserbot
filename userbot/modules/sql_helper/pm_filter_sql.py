4eb52d28fdf1ad8f95ac8be8d7105932f3c5cabe
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2025-03-10 00:39:26 +0400

    Yeni özəllik😍

--- userbot/modules/sql_helper/pm_filter_sql.py
+++ userbot/modules/sql_helper/pm_filter_sql.py
@@ -7,11 +7,12 @@ from sqlalchemy import Column, String, UnicodeText, Integer
 
 class PMFilters(BASE):
     __tablename__ = "pm_filters"
-    keyword = Column(String(50), primary_key=True)
-    reply = Column(UnicodeText)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    keyword = Column(String(50), unique=True, nullable=False)
+    reply = Column(UnicodeText, nullable=False)
     f_mesg_id = Column(Integer, nullable=True)
 
-    def __init__(self, keyword, reply, f_mesg_id):
+    def __init__(self, keyword, reply, f_mesg_id=None):
         self.keyword = keyword
         self.reply = reply
         self.f_mesg_id = f_mesg_id
@@ -19,9 +20,10 @@ class PMFilters(BASE):
 PMFilters.__table__.create(checkfirst=True)
 
 def add_pm_filter(keyword, reply, f_mesg_id=None):
-    filter_exists = SESSION.query(PMFilters).get(keyword)
-    if filter_exists:
-        SESSION.delete(filter_exists)
+    existing_filter = SESSION.query(PMFilters).filter_by(keyword=keyword).first()
+    if existing_filter:
+        SESSION.delete(existing_filter)
+
     new_filter = PMFilters(keyword, reply, f_mesg_id)
     SESSION.add(new_filter)
     SESSION.commit()
@@ -30,9 +32,9 @@ def get_pm_filters():
     return SESSION.query(PMFilters).all()
 
 def remove_pm_filter(keyword):
-    filter_exists = SESSION.query(PMFilters).get(keyword)
-    if filter_exists:
-        SESSION.delete(filter_exists)
+    filter_to_remove = SESSION.query(PMFilters).filter_by(keyword=keyword).first()
+    if filter_to_remove:
+        SESSION.delete(filter_to_remove)
         SESSION.commit()
         return True
     return False
