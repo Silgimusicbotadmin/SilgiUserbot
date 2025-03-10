66cf7ae3b17ec5fb2922c7d64c62c57025116593
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2025-03-10 21:17:45 +0400

    Yeni ozellik

---
+++ userbot/modules/sql_helper/pm_filters_sql.py
@@ -0,0 +1,58 @@
+from sqlalchemy import Column, UnicodeText, Numeric, String
+from userbot.modules.sql_helper import BASE, SESSION
+
+class PMMessageFilters(BASE):
+    __tablename__ = "pm_message_filters"  
+    user_id = Column(String(14), primary_key=True)
+    keyword = Column(UnicodeText, primary_key=True, nullable=False)
+    reply = Column(UnicodeText)
+    f_mesg_id = Column(Numeric)
+
+    def __init__(self, user_id, keyword, reply, f_mesg_id):
+        self.user_id = str(user_id)
+        self.keyword = keyword
+        self.reply = reply
+        self.f_mesg_id = f_mesg_id
+
+    def __eq__(self, other):
+        return bool(
+            isinstance(other, PMMessageFilters) and self.user_id == other.user_id
+            and self.keyword == other.keyword)
+
+PMMessageFilters.__table__.create(checkfirst=True)
+
+def get_pm_filter(user_id, keyword):
+    try:
+        return SESSION.query(PMMessageFilters).get((str(user_id), keyword))
+    finally:
+        SESSION.close()
+def get_pm_filters(user_id):
+    try:
+        return SESSION.query(PMMessageFilters).filter(
+            PMMessageFilters.user_id == str(user_id)).all()
+    finally:
+        SESSION.close()
+def add_pm_filter(user_id, keyword, reply, f_mesg_id):
+    to_check = get_pm_filter(user_id, keyword)
+    if not to_check:
+        adder = PMMessageFilters(str(user_id), keyword, reply, f_mesg_id)
+        SESSION.add(adder)
+        SESSION.commit()
+        return True
+    else:
+        rem = SESSION.query(PMMessageFilters).get((str(user_id), keyword))
+        SESSION.delete(rem)
+        SESSION.commit()
+        adder = PMMessageFilters(str(user_id), keyword, reply, f_mesg_id)
+        SESSION.add(adder)
+        SESSION.commit()
+        return False
+def remove_pm_filter(user_id, keyword):
+    to_check = get_pm_filter(user_id, keyword)
+    if not to_check:
+        return False
+    else:
+        rem = SESSION.query(PMMessageFilters).get((str(user_id), keyword))
+        SESSION.delete(rem)
+        SESSION.commit()
+        return True
