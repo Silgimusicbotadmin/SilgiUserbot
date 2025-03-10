cfa019f921417fad3af6ecd6f1b7b74cf3fc7faf
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2025-03-10 21:50:34 +0400

    Update pm_filters_sql.py

--- userbot/modules/sql_helper/pm_filters_sql.py
+++ userbot/modules/sql_helper/pm_filters_sql.py
@@ -3,56 +3,56 @@ from userbot.modules.sql_helper import BASE, SESSION
 
 class PMMessageFilters(BASE):
     __tablename__ = "pm_message_filters"  
-    user_id = Column(String(14), primary_key=True)
+    chat_id = Column(String(14), primary_key=True)
     keyword = Column(UnicodeText, primary_key=True, nullable=False)
     reply = Column(UnicodeText)
     f_mesg_id = Column(Numeric)
 
-    def __init__(self, user_id, keyword, reply, f_mesg_id):
-        self.user_id = str(user_id)
+    def __init__(self, chat_id, keyword, reply, f_mesg_id):
+        self.user_id = str(chat_id)
         self.keyword = keyword
         self.reply = reply
         self.f_mesg_id = f_mesg_id
 
     def __eq__(self, other):
         return bool(
-            isinstance(other, PMMessageFilters) and self.user_id == other.user_id
+            isinstance(other, PMMessageFilters) and self.chat_id == other.chat_id
             and self.keyword == other.keyword)
 
 PMMessageFilters.__table__.create(checkfirst=True)
 
-def get_pm_filter(user_id, keyword):
+def get_pm_filter(chat_id, keyword):
     try:
-        return SESSION.query(PMMessageFilters).get((str(user_id), keyword))
+        return SESSION.query(PMMessageFilters).get((str(chat_id), keyword))
     finally:
         SESSION.close()
-def get_pm_filters(user_id):
+def get_pm_filters(chat_id):
     try:
         return SESSION.query(PMMessageFilters).filter(
-            PMMessageFilters.user_id == str(user_id)).all()
+            PMMessageFilters.chat_id == str(chat_id)).all()
     finally:
         SESSION.close()
-def add_pm_filter(user_id, keyword, reply, f_mesg_id):
-    to_check = get_pm_filter(user_id, keyword)
+def add_pm_filter(chat_id, keyword, reply, f_mesg_id):
+    to_check = get_pm_filter(chat_id, keyword)
     if not to_check:
-        adder = PMMessageFilters(str(user_id), keyword, reply, f_mesg_id)
+        adder = PMMessageFilters(str(chat_id), keyword, reply, f_mesg_id)
         SESSION.add(adder)
         SESSION.commit()
         return True
     else:
-        rem = SESSION.query(PMMessageFilters).get((str(user_id), keyword))
+        rem = SESSION.query(PMMessageFilters).get((str(chat_id), keyword))
         SESSION.delete(rem)
         SESSION.commit()
-        adder = PMMessageFilters(str(user_id), keyword, reply, f_mesg_id)
+        adder = PMMessageFilters(str(chat_id), keyword, reply, f_mesg_id)
         SESSION.add(adder)
         SESSION.commit()
         return False
-def remove_pm_filter(user_id, keyword):
-    to_check = get_pm_filter(user_id, keyword)
+def remove_pm_filter(chat_id, keyword):
+    to_check = get_pm_filter(chat_id, keyword)
     if not to_check:
         return False
     else:
-        rem = SESSION.query(PMMessageFilters).get((str(user_id), keyword))
+        rem = SESSION.query(PMMessageFilters).get((str(chat_id), keyword))
         SESSION.delete(rem)
         SESSION.commit()
         return True
