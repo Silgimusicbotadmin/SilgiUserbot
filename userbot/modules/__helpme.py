c88ece72ee1c00398e3f175965e3fb8f78357578
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2025-03-12 13:55:26 +0400

    Update __helpme.py

--- userbot/modules/__helpme.py
+++ userbot/modules/__helpme.py
@@ -16,7 +16,7 @@ async def yardim(event):
     if tgbotusername is not None:
         results = await event.client.inline_query(
             tgbotusername,
-            "kömek"
+            "@SilgiUB"
         )
         await results[0].click(
             event.chat_id,
