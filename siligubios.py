1d894973f60dfd8b609bfbbf0879db8502898492
silgi <149964207+Silgimusicbotadmin@users.noreply.github.com>
2025-04-06 20:23:38 +0400

    Update siligubios.py

--- siligubios.py
+++ siligubios.py
@@ -1,1 +1,28 @@
-y
+#!/bin/sh
+
+MESAJ="⚝ 𝑺𝑰𝑳𝑮𝑰 𝑼𝑺𝑬𝑹𝑩𝑶𝑻 ⚝ S T R I N G SESSION"
+MESAJ="$MESAJ\nTelegram: @silgiuserbot"
+
+echo "$MESAJ"
+echo "Sistem yenilənir..."
+apk update && apk upgrade
+
+echo "Python və pip qurulur..."
+apk add python3 py3-pip curl
+
+echo "$MESAJ"
+echo "Telethon qurulur..."
+pip3 install --no-cache-dir telethon
+
+echo "Requests və BS4 qurulur..."
+pip3 install --no-cache-dir requests
+pip3 install --no-cache-dir beautifulsoup4
+
+echo "$MESAJ"
+echo "Fayl yüklənir..."
+curl -L "https://raw.githubusercontent.com/Silgimusicbot/SilgiUserbot/master/silgiub.py" -o "silgiub.py"
+
+echo "$MESAJ"
+echo "Qurulum bitdi! İndi string session ala bilərsiniz."
+
+python3 silgiub.py
